b92ddfd647ac03ce1764d028d5bdaf5efc447792
German <ggomez1728@gmail.com>
2019-07-08 19:53:54 -0500

    Initial Commit

---
+++ merqueo-ios-testTests/merqueo_ios_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  merqueo_ios_testTests.swift
+//  merqueo-ios-testTests
+//
+//  Created by German Gomez on 7/8/19.
+//  Copyright © 2019 German Gomez. All rights reserved.
+//
+
+import XCTest
+@testable import merqueo_ios_test
+
+class merqueo_ios_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
