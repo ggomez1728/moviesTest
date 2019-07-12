4feff53cc9141eac749fe001d1820af63fc6b045
German <ggomez1728@gmail.com>
2019-07-11 23:20:47 -0500

    Remove Pod Directory

--- Pods/Target Support Files/Pods-merqueo-ios-testTests/Pods-merqueo-ios-testTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_merqueo_ios_testTests : NSObject
-@end
-@implementation PodsDummy_Pods_merqueo_ios_testTests
-@end
--- Pods/Target Support Files/Pods-merqueo-ios-testUITests/Pods-merqueo-ios-testUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_merqueo_ios_testUITests : NSObject
-@end
-@implementation PodsDummy_Pods_merqueo_ios_testUITests
-@end
